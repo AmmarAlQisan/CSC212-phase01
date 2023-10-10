535d2fa0b3fcc3f068cafa549d9064b0c6c61f1c
ammar <ammar@localhost>
2023-10-10 22:38:29 +0300

    adding insertion

--- CSC212/src/Phase01/LinkedListADT.java
+++ CSC212/src/Phase01/LinkedListADT.java
@@ -9,38 +9,73 @@ public class LinkedListADT <T>{
 	      size = 0;  
 	      head = current = null;  
      }  
+	 
+	 
 	public boolean isEmpty () {  
 	      return head == null;  
 	 }  
+	
+	
 	 public boolean last () {  
 	       return current.next == null;  
 	 }  
+	 
+	 
 	 public boolean full () {  
 	        return false;  
 	 }  
+	 
+	 
 	 public void findFirst () {  
 	         current = head;  
 	 }  
+	 
+	 
 	 public void findNext () {  
 	         current = current.next;  
 	 }  
 	 public T retrieve () {  
 	         return current.data;  
 	 }  
+	 
+	 
 	 public void update (T val) {  
 	         current.data = val;  
 	 }  
+	 
+	 
 	 public boolean insertionSort(T val) {
-		 return false;
+		 Node<T> tmp;
+		 
+		 if(isEmpty()) {
+			 current = head = new Node<T> (val);
+			 return false;
+			 
+		 }else {
+			 tmp = current.next;
+			 current.next = new Node<T> (val);
+			 current = current.next;
+			 current.next = tmp;
+			 return true;
+		 } 
 
 	 }
+	 
+	 
+	 
 	  public boolean searching(T val) {
 		  return false;
 	  }
+	  
+	  
 	  public T remove (T val) { 
 		  return val;
 	  }
+	  
+	  
 	  public void printInformation() {
 		  
 	  }
+	  
+	  
 }
