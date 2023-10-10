518d1d4f760843a2f4f28405dd3babc0f6442fde
yooyo <yooyo@Yaqoub>
2023-10-10 20:21:33 +0300

    add

--- CSC212/src/Phase01/Contact.java
+++ CSC212/src/Phase01/Contact.java
@@ -9,94 +9,56 @@ public class Contact implements Comparable<Contact> {
 	public String birthday;
 	public String notes;
 	
-	
-	
-	
 	public String getName() {
 		return name;
 	}
-
-
-
-
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
-
 
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
-
-
-
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-
-
-
 
 	public String getEmailAddress() {
 		return emailAddress;
 	}
 
-
-
-
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
-
-
-
 
 	public String getAddress() {
 		return address;
 	}
 
-
-
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
-
-
 
 	public String getBirthday() {
 		return birthday;
 	}
 
-
-
-
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
-
-
-
 
 	public String getNotes() {
 		return notes;
 	}
 
-
-
-
 	public void setNotes(String notes) {
 		this.notes = notes;
 	}
 
-
-
 // constructor
-	
+
 	public Contact(String name, String phoneNumber, String emailAddress, 
 			String address, String birthday,String notes) {
 		this.name = name;
@@ -106,9 +68,6 @@ public class Contact implements Comparable<Contact> {
 		this.birthday = birthday;
 		this.notes = notes;
 	}
-
-
-
 
 	@Override
 	public int compareTo(Contact o) {
